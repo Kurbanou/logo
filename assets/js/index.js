767b6527f705c48cdff6fc8bcbfc076eb58ba91c
Farid Kurbanou <kurbanou.faridun@gmail.com>
2022-09-04 16:24:11 +0300

    feat: js modify

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,31 @@
 'use strict';
-const popup = document.querySelector('.popup');  // .gameinfo для опред попапа , если попапов больше => дублировать функции с их именами
+const popup = document.querySelector('.popup');
 const popBody = document.querySelector('.popup__content');
 const popCls = document.querySelector('.x');
 const form = document.querySelector('.form');
 const button = document.querySelector('.button');
+const input = document.querySelector('.input');
+const fields = document.querySelectorAll('.field');
 
 
+fields.forEach(field => {
+    field.addEventListener('dblclick', () => {
+        openPopup();
+        field.classList.add('modify');
+        input.innerHTML = field.innerHTML;
+    })
+})
+
+
+button.addEventListener('click', () => {
+    const modify = document.querySelector('.modify');
+    modify.innerHTML = input.value;
+    modify.classList.remove('modify');
+    clsPopup();
+})
 
 
 popup.addEventListener('click', e => { //закрываем popup
-
     let target = e.target;
     let its_popup = target == popBody || popBody.contains(target);
     let its_button = target == popCls;
@@ -18,54 +34,19 @@ popup.addEventListener('click', e => { //закрываем popup
         clsPopup();
 
     }
-
 })
-
 
 
 function openPopup() {
-
     popup.classList.add('popup_open');
     document.body.classList.add('open');
     popBody.style.transitionDelay = `0.9s`;
-
 }
-
 
 
 function clsPopup() {
-
     popup.classList = 'popup';
     document.body.classList = '';
     popBody.style.transitionDelay = '0s';
-
+    form.reset();
 }
-
-
-
-
-
-
-
-
-document.addEventListener('dblclick', e => {
-
-
-    if (e.target.classList.contains('field')) {
-        openPopup();
-        let target = e.target;
-        let input = document.querySelector('.input');
-
-        button.addEventListener('click', () => {
-
-            target.innerHTML = input.value;
-            clsPopup();
-            form.reset();
-            return;
-        })
-
-    } else {
-        return false;
-    }
-
-})
